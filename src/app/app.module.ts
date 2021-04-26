d30c6af075b0ba4433e47d6e4ca4d96ffa60abe2
Vinicius Cardoso <vinicius.cardoso@sapiensia.com>
2021-04-25 21:38:22 -0300

    Last posts section; breaking sections into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannersComponent } from './components/banners/banners.component';
+import { ChannelsComponent } from './components/channels/channels.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LastPostsComponent } from './components/last-posts/last-posts.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        HeaderComponent,
+        BannersComponent,
+        LastPostsComponent,
+        ChannelsComponent,
+        FooterComponent,
+
+        AppComponent
+    ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/components/last-posts/last-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-last-posts',
+    templateUrl: './last-posts.component.html',
+    styleUrls: ['./last-posts.component.scss']
+})
+export class LastPostsComponent {
+
+}
