822aed5b543f2f06371186d563429b9d4aaf2bac
Vinicius Cardoso <vinicius.cardoso@sapiensia.com>
2021-05-18 20:50:48 -0300

    Banner carousel done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,11 @@ import { LastPostsComponent } from './components/last-posts/last-posts.component
 
         AppComponent
     ],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        IvyCarouselModule
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/components/banners/banners.component.ts
+++ src/app/components/banners/banners.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-banners',
     templateUrl: './banners.component.html',
     styleUrls: ['./banners.component.scss']
 })
-export class BannersComponent {
+export class BannersComponent implements AfterViewInit {
 
+    @ViewChild('slider') public slider: any;
+
+    public images: Array<any> = [
+        { path: 'https://picsum.photos/id/1000/1920/1080', href: '/' },
+        { path: 'https://picsum.photos/id/1002/1920/1080', href: '/' },
+        { path: 'https://picsum.photos/id/1021/1920/1080', href: '/' }
+    ];
+
+    constructor() {}
+
+    ngAfterViewInit() {
+        for(const cell of this.slider.cells.cells) {
+            const href = this.images.find(x => x.path === cell.firstChild.src).href;
+            cell.onclick = () => window.location.href = href;
+        }
+    }
 }
