f3b0fbafc51e8b5ed38e4267ff6758752f15d96c
Vinicius Cardoso <vinicius.cardoso@sapiensia.com>
2021-08-29 21:50:51 -0300

    More bugfixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    @HostListener('window:resize', ['$event'])
-    onResize(event: any) {
-        window.location.reload();
-    }
-}
+export class AppComponent {}
